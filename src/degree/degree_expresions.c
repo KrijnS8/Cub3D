738e5a157b42091343ab4022839c9e4bc0b2b7f5
Krijn Schelvis <kschelvi@f1r2s8.codam.nl>
2024-09-24 16:40:00 +0200

    degree

--- src/degree/degree_expresions.c
+++ src/degree/degree_expresions.c
@@ -6,7 +6,7 @@
 /*   By: kschelvi <kschelvi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/09/24 16:10:02 by kschelvi      #+#    #+#                 */
-/*   Updated: 2024/09/24 16:32:18 by kschelvi      ########   odam.nl         */
+/*   Updated: 2024/09/24 16:39:50 by kschelvi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_degree	degree_add(t_degree d1, t_degree d2)
 	t_degree result;
 	
     result.value = fmod(d1.value + d2.value, 360.0);
-    return result;
+    return (result);
 }
 
 t_degree	degree_sub(t_degree d1, t_degree d2)
@@ -26,7 +26,7 @@ t_degree	degree_sub(t_degree d1, t_degree d2)
 	t_degree result;
 	
     result.value = fmod(d1.value - d2.value + 360.0, 360.0);
-    return result;
+    return (result);
 }
 
 t_degree	degree_multi(t_degree d1, t_degree d2)
@@ -34,13 +34,16 @@ t_degree	degree_multi(t_degree d1, t_degree d2)
 	t_degree result;
 	
     result.value = fmod(d1.value * d2.value, 360.0);
-    return result;
+    return (result);
 }
 
 t_degree	degree_div(t_degree d1, t_degree d2)
 {
 	t_degree result;
+
+	if (d2.value == 0)
+		return (int_to_degree(0));
 	
     result.value = fmod(d1.value / d2.value + 360, 360.0);
-    return result;
+    return (result);
 }
