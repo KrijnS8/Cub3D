5425d80b61df2e88aad012953b69a8abd4292712
Krijn Schelvis <kschelvi@f0r1s13.codam.nl>
2024-10-21 17:14:26 +0200

    finally getting somewhere

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: splattje <splattje@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/14 10:10:37 by splattje      #+#    #+#                 */
-/*   Updated: 2024/10/21 16:42:53 by kschelvi      ########   odam.nl         */
+/*   Updated: 2024/10/21 17:06:37 by kschelvi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,8 @@ int	main(int argc, char **argv)
 	mlx_hook(data->win, KeyRelease, KeyReleaseMask, &handle_release, data);
 	mlx_hook(data->win, DestroyNotify, StructureNotifyMask, &des_func, data);
 	mlx_mouse_move(data->mlx, data->win, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-	//mlx_loop_hook(data->mlx, &build_frame, data);
-	build_frame(data);
+	mlx_loop_hook(data->mlx, &build_frame, data);
+	//build_frame(data);
 	mlx_loop(data->mlx);
 	mlx_do_key_autorepeaton(data->mlx);
 	free_data(data);
--- src/rendering/rendering.c
+++ src/rendering/rendering.c
@@ -6,7 +6,7 @@
 /*   By: kschelvi <kschelvi@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/15 15:11:48 by kschelvi      #+#    #+#                 */
-/*   Updated: 2024/10/21 16:48:08 by kschelvi      ########   odam.nl         */
+/*   Updated: 2024/10/21 17:13:11 by kschelvi      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,52 @@
 #include "my_error.h"
 #include <math.h>
 
-void	render_wall(t_data *data, t_ray *rays, int n)
+static int	get_line_width()
+{
+	static int	line_width = -1;
+
+	if (line_width == -1)
+		line_width = SCREEN_WIDTH / get_num_rays();
+	return (line_width + 1);
+}
+
+void	render_ray(t_data *data, t_ray ray, int n_rays, int i, int location_rays)
+{
+	int		x;
+	int		y;
+	int		dx;
+	int		dy;
+	double	scale_factor;
+
+	x = get_line_width() * (location_rays + i);
+	y = (SCREEN_HEIGHT - ray.height) / 2;
+	scale_factor = (double)data->map->img[ray.index].h / ray.height;
+	dx = 0;
+	dy = 0;
+	while (dx < get_line_width())
+	{
+		while (dy < ray.height)
+		{
+			int	src_x = (data->map->img[ray.index].w / n_rays * i) + dx;
+			int src_y = (int)dy * scale_factor;
+			src_x = src_x % data->map->img[ray.index].w;
+            src_y = src_y % data->map->img[ray.index].h;
+			put_pixel_img(data->frame, x + dx, y + dy, get_pixel_img(data->map->img[ray.index], src_x, src_y));
+			dy++;
+		}
+		dx++;
+	}
+}
+
+void	render_wall(t_data *data, t_ray *rays, int n_rays, int location_rays)
 {
 	(void)data;
 	int	i;
 
 	i = 0;
-	while (i < n)
+	while (i < n_rays)
 	{
-		printf("map_x: %d, map_y: %d\n", rays[i].map_x, rays[i].map_y);
+		render_ray(data, rays[i], n_rays, i ,location_rays);
 		i++;
 	}
 }
@@ -42,7 +79,7 @@ t_error	render_frame(t_data *data, t_ray *rays)
 		wall.y = rays[i].map_y;
 		while (rays[i + j].map_x == wall.x && rays[i + j].map_y == wall.y)
 			j++;
-		render_wall(data, &(rays[i]), j);
+		render_wall(data, &(rays[i]), j, i);
 		i += j;
 	}
 	return (ERR_OK);
